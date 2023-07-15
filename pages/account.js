9e6d93757a21857da21c5cddfa4f63b5d2152125
abelmuro <abelmuro93@gmail.com>
2023-07-15 13:55:39 -0700

    commit

--- pages/account.js
+++ pages/account.js
@@ -25,6 +25,7 @@ export default function Account() {
                     uid && <LinksTab/>
                     : uid && <ProfileTab/>}
             </main> 
+        
             <ChangesSavedDialog/>          
         </>
 
