c194799039bfa6d0c7d041b7254d31d8bf679c6f
abel muro <abelmuro93@gmail.com>
2023-11-01 20:16:07 -0700

    commit

--- firebase/Configuration.js
+++ firebase/Configuration.js
@@ -7,12 +7,13 @@ const firebaseConfig = {
   apiKey: process.env.NEXT_PUBLIC_apiKey,
   authDomain: process.env.NEXT_PUBLIC_authDomain,
   projectId: process.env.NEXT_PUBLIC_projectId,
-  storageBucket: process.env.NEXT_PUBLIC_messagingSenderId,
+  storageBucket: process.env.NEXT_PUBLIC_storageBucket,
+  messagingSenderId: process.env.NEXT_PUBLIC_messagingSenderId,
   appId: process.env.NEXT_PUBLIC_appId
 };
 
 // Initialize Firebase
-const app = initializeApp(firebaseConfig);
+const app = initializeApp(firebaseConfig, 'new instance');
 export const db = getFirestore(app);
 export const auth = getAuth(app);    
 export const storage = getStorage(app, process.env.NEXT_PUBLIC_storage);
